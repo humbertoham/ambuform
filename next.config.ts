1820fc5eaba5cbe2dddb72498564e6e26a10b678
humbertoham <hamduranhumberto@gmail.com>
2025-05-22 15:38:14 -0600

    final version 2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-// next.config.ts
-import withPWA from '@ducanh2912/next-pwa'
 import type { NextConfig } from 'next'
+import withPWA from '@ducanh2912/next-pwa'
+import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 const pwaOptions = {
   dest: 'public',
@@ -8,26 +8,26 @@ const pwaOptions = {
   skipWaiting: true,
   runtimeCaching: [
     {
+      // Cache de los archivos estáticos de Next (_next)
       urlPattern: /^\/_next\/static\/.*/i,
-      handler: 'StaleWhileRevalidate',
+      handler: StaleWhileRevalidate,
       options: {
         cacheName: 'static-resources',
       },
     },
     {
+      // Cache de tu CSS/JS/build output
       urlPattern: /^\/_next\/image\/.*/i,
-      handler: 'CacheFirst',
+      handler: CacheFirst,
       options: {
         cacheName: 'images',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60,
-        },
+        expiration: { maxEntries: 50, maxAgeSeconds: 30 * 24 * 60 * 60 },
       },
     },
     {
+      // Cache de las rutas de tu app (páginas)
       urlPattern: /^\/.*$/i,
-      handler: 'NetworkFirst',
+      handler: NetworkFirst,
       options: {
         cacheName: 'pages',
         networkTimeoutSeconds: 3,
@@ -38,6 +38,8 @@ const pwaOptions = {
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // si quieres deshabilitar en dev:
+  // pwa: { disable: process.env.NODE_ENV === 'development' }
 }
 
 export default withPWA(pwaOptions)(nextConfig)
